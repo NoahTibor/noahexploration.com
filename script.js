6d5e1ac154fd0e923b03a642eff79060361c0a71
Noah-Tibor <tibornoah1@gmail.com>
2025-07-13 04:13:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// Mobile menu toggle
 document.querySelector('.hamburger').addEventListener('click', function() {
     document.querySelector('.sidebar').classList.toggle('active');
+});
+
+// Close menu when clicking on a link
+document.querySelectorAll('.sidebar a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.sidebar').classList.remove('active');
+    });
 });
